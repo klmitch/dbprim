927f494879dfade3135972d9bfa05e94bde8ac0a
Kevin L. Mitchell <klmitch@mit.edu>
2017-10-21 13:02:36 -0500

    First pass at bringing tests back into action
    This change represents a first pass at bringing the existing tests into
    the action.

--- tests/t_hashtab.c
+++ tests/t_hashtab.c
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2004 by Kevin L. Mitchell <klmitch@mit.edu>
+** Copyright (C) 2004, 2017 by Kevin L. Mitchell <klmitch@mit.edu>
 **
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Library General Public
@@ -15,8 +15,6 @@
 ** License along with this library; if not, write to the Free
 ** Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ** MA 02111-1307, USA
-**
-** @(#)$Id$
 */
 #include <stdio.h>
 #include <stdlib.h>
@@ -27,8 +25,6 @@
 
 #include "dbprim.h"
 #include "dbprim_int.h"
-
-RCSTAG("@(#)$Id$");
 
 TEST_PROG(t_hashtab, "Test hash table functionality")
 
--- tests/test-harness.h
+++ tests/test-harness.h
@@ -1,5 +1,5 @@
 /*
-** Copyright (C) 2004 by Kevin L. Mitchell <klmitch@mit.edu>
+** Copyright (C) 2004, 2017 by Kevin L. Mitchell <klmitch@mit.edu>
 **
 ** This library is free software; you can redistribute it and/or
 ** modify it under the terms of the GNU Library General Public
@@ -15,8 +15,6 @@
 ** License along with this library; if not, write to the Free
 ** Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
 ** MA 02111-1307, USA
-**
-** @(#)$Id$
 */
 #ifndef __include_test_harness_h__
 #define __include_test_harness_h__
