754228ba440c08ffea6a61aeb585f86aa52af0f3
Kevin L. Mitchell <klmitch@mit.edu>
2017-10-29 09:39:04 -0500

    Add tests on redblack internal routines
    This commit adds the tests for the internal redblack routines,
    _rb_locate() and _rb_rotate().

--- tests/test__rb_locate.c
+++ tests/test__rb_locate.c
@@ -23,10 +23,222 @@
 
 #include "redblack_int.h"
 
+void
+test_empty_nocreate(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t *result;
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  result = _rb_locate(&tree, 0, &key);
+
+  assert_ptr_equal(result, 0);
+  assert_ptr_equal(tree.rt_root, 0);
+  assert_int_equal(tree.rt_count, 0);
+}
+
+void
+test_empty_create(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t node = RB_NODE_INIT(0), *result;
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  result = _rb_locate(&tree, &node, &key);
+
+  assert_ptr_equal(result, &node);
+  assert_ptr_equal(tree.rt_root, &node);
+  assert_int_equal(tree.rt_count, 1);
+  assert_int_equal(node.rn_color, RB_COLOR_BLACK);
+  assert_ptr_equal(node.rn_tree, &tree);
+  assert_ptr_equal(node.rn_parent, 0);
+  assert_ptr_equal(node.rn_left, 0);
+  assert_ptr_equal(node.rn_right, 0);
+  assert_ptr_equal(node.rn_key.dk_key, key.dk_key);
+  assert_int_equal(node.rn_key.dk_len, key.dk_len);
+}
+
+void
+test_exists_root(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t root = RB_NODE_INIT(0), node = RB_NODE_INIT(0), *result;
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  tree.rt_root = &root;
+  tree.rt_count = 1;
+  root.rn_color = RB_COLOR_BLACK;
+  root.rn_tree = &tree;
+  root.rn_key = key;
+
+  result = _rb_locate(&tree, &node, &key);
+
+  assert_ptr_equal(result, &root);
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_int_equal(tree.rt_count, 1);
+  assert_int_equal(node.rn_color, RB_COLOR_NONE);
+  assert_ptr_equal(node.rn_tree, 0);
+  assert_ptr_equal(node.rn_parent, 0);
+  assert_ptr_equal(node.rn_left, 0);
+  assert_ptr_equal(node.rn_right, 0);
+  assert_ptr_equal(node.rn_key.dk_key, 0);
+  assert_int_equal(node.rn_key.dk_len, 0);
+}
+
+void
+test_find_in_tree(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t root = RB_NODE_INIT(0), node = RB_NODE_INIT(0), *result;
+  rb_node_t rank1_0 = RB_NODE_INIT(0), rank2_1 = RB_NODE_INIT(0);
+  db_key_t aaaa = DB_KEY_INIT("aaaa", 4), aaab = DB_KEY_INIT("aaab", 4);
+  db_key_t aaac = DB_KEY_INIT("aaac", 4);
+
+  tree.rt_root = &root;
+  tree.rt_count = 3;
+  root.rn_color = RB_COLOR_BLACK;
+  root.rn_tree = &tree;
+  root.rn_key = aaac;
+  root.rn_left = &rank1_0;
+  rank1_0.rn_color = RB_COLOR_RED;
+  rank1_0.rn_tree = &tree;
+  rank1_0.rn_parent = &root;
+  rank1_0.rn_key = aaaa;
+  rank1_0.rn_right = &rank2_1;
+  rank2_1.rn_color = RB_COLOR_BLACK;
+  rank2_1.rn_tree = &tree;
+  rank2_1.rn_parent = &rank1_0;
+  rank2_1.rn_key = aaab;
+
+  result = _rb_locate(&tree, &node, &aaab);
+
+  assert_ptr_equal(result, &rank2_1);
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_int_equal(tree.rt_count, 3);
+  assert_ptr_equal(root.rn_parent, 0);
+  assert_ptr_equal(root.rn_left, &rank1_0);
+  assert_ptr_equal(root.rn_right, 0);
+  assert_ptr_equal(rank1_0.rn_parent, &root);
+  assert_ptr_equal(rank1_0.rn_left, 0);
+  assert_ptr_equal(rank1_0.rn_right, &rank2_1);
+  assert_ptr_equal(rank2_1.rn_parent, &rank1_0);
+  assert_ptr_equal(rank2_1.rn_left, 0);
+  assert_ptr_equal(rank2_1.rn_right, 0);
+  assert_int_equal(node.rn_color, RB_COLOR_NONE);
+  assert_ptr_equal(node.rn_tree, 0);
+  assert_ptr_equal(node.rn_parent, 0);
+  assert_ptr_equal(node.rn_left, 0);
+  assert_ptr_equal(node.rn_right, 0);
+  assert_ptr_equal(node.rn_key.dk_key, 0);
+  assert_int_equal(node.rn_key.dk_len, 0);
+}
+
+void
+test_add_left(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t root = RB_NODE_INIT(0), node = RB_NODE_INIT(0), *result;
+  rb_node_t rank1_0 = RB_NODE_INIT(0), rank2_1 = RB_NODE_INIT(0);
+  db_key_t aaaa = DB_KEY_INIT("aaaa", 4), aaab = DB_KEY_INIT("aaab", 4);
+  db_key_t aaac = DB_KEY_INIT("aaac", 4), aaae = DB_KEY_INIT("aaae", 4);
+
+  tree.rt_root = &root;
+  tree.rt_count = 3;
+  root.rn_color = RB_COLOR_BLACK;
+  root.rn_tree = &tree;
+  root.rn_key = aaae;
+  root.rn_left = &rank1_0;
+  rank1_0.rn_color = RB_COLOR_RED;
+  rank1_0.rn_tree = &tree;
+  rank1_0.rn_parent = &root;
+  rank1_0.rn_key = aaaa;
+  rank1_0.rn_right = &rank2_1;
+  rank2_1.rn_color = RB_COLOR_BLACK;
+  rank2_1.rn_tree = &tree;
+  rank2_1.rn_parent = &rank1_0;
+  rank2_1.rn_key = aaac;
+
+  result = _rb_locate(&tree, &node, &aaab);
+
+  assert_ptr_equal(result, &node);
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_int_equal(tree.rt_count, 4);
+  assert_ptr_equal(root.rn_parent, 0);
+  assert_ptr_equal(root.rn_left, &rank1_0);
+  assert_ptr_equal(root.rn_right, 0);
+  assert_ptr_equal(rank1_0.rn_parent, &root);
+  assert_ptr_equal(rank1_0.rn_left, 0);
+  assert_ptr_equal(rank1_0.rn_right, &rank2_1);
+  assert_ptr_equal(rank2_1.rn_parent, &rank1_0);
+  assert_ptr_equal(rank2_1.rn_left, &node);
+  assert_ptr_equal(rank2_1.rn_right, 0);
+  assert_int_equal(node.rn_color, RB_COLOR_RED);
+  assert_ptr_equal(node.rn_tree, &tree);
+  assert_ptr_equal(node.rn_parent, &rank2_1);
+  assert_ptr_equal(node.rn_left, 0);
+  assert_ptr_equal(node.rn_right, 0);
+  assert_ptr_equal(node.rn_key.dk_key, aaab.dk_key);
+  assert_int_equal(node.rn_key.dk_len, aaab.dk_len);
+}
+
+void
+test_add_right(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(rbtree_comp, 0);
+  rb_node_t root = RB_NODE_INIT(0), node = RB_NODE_INIT(0), *result;
+  rb_node_t rank1_0 = RB_NODE_INIT(0), rank2_1 = RB_NODE_INIT(0);
+  db_key_t aaaa = DB_KEY_INIT("aaaa", 4), aaad = DB_KEY_INIT("aaad", 4);
+  db_key_t aaac = DB_KEY_INIT("aaac", 4), aaae = DB_KEY_INIT("aaae", 4);
+
+  tree.rt_root = &root;
+  tree.rt_count = 3;
+  root.rn_color = RB_COLOR_BLACK;
+  root.rn_tree = &tree;
+  root.rn_key = aaae;
+  root.rn_left = &rank1_0;
+  rank1_0.rn_color = RB_COLOR_RED;
+  rank1_0.rn_tree = &tree;
+  rank1_0.rn_parent = &root;
+  rank1_0.rn_key = aaaa;
+  rank1_0.rn_right = &rank2_1;
+  rank2_1.rn_color = RB_COLOR_BLACK;
+  rank2_1.rn_tree = &tree;
+  rank2_1.rn_parent = &rank1_0;
+  rank2_1.rn_key = aaac;
+
+  result = _rb_locate(&tree, &node, &aaad);
+
+  assert_ptr_equal(result, &node);
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_int_equal(tree.rt_count, 4);
+  assert_ptr_equal(root.rn_parent, 0);
+  assert_ptr_equal(root.rn_left, &rank1_0);
+  assert_ptr_equal(root.rn_right, 0);
+  assert_ptr_equal(rank1_0.rn_parent, &root);
+  assert_ptr_equal(rank1_0.rn_left, 0);
+  assert_ptr_equal(rank1_0.rn_right, &rank2_1);
+  assert_ptr_equal(rank2_1.rn_parent, &rank1_0);
+  assert_ptr_equal(rank2_1.rn_left, 0);
+  assert_ptr_equal(rank2_1.rn_right, &node);
+  assert_int_equal(node.rn_color, RB_COLOR_RED);
+  assert_ptr_equal(node.rn_tree, &tree);
+  assert_ptr_equal(node.rn_parent, &rank2_1);
+  assert_ptr_equal(node.rn_left, 0);
+  assert_ptr_equal(node.rn_right, 0);
+  assert_ptr_equal(node.rn_key.dk_key, aaad.dk_key);
+  assert_int_equal(node.rn_key.dk_len, aaad.dk_len);
+}
+
 int
 main(void)
 {
   const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_empty_nocreate),
+    cmocka_unit_test(test_empty_create),
+    cmocka_unit_test(test_exists_root),
+    cmocka_unit_test(test_find_in_tree),
+    cmocka_unit_test(test_add_left),
+    cmocka_unit_test(test_add_right)
   };
 
   return cmocka_run_group_tests_name("Test _rb_locate.c", tests, 0, 0);
--- tests/test__rb_rotate.c
+++ tests/test__rb_rotate.c
@@ -23,10 +23,228 @@
 
 #include "redblack_int.h"
 
+void
+test_noparent(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t child = RB_NODE_INIT(0);
+  rb_node_t left = RB_NODE_INIT(0), right = RB_NODE_INIT(0);
+
+  tree.rt_root = &child;
+  child.rn_left = &left;
+  child.rn_right = &right;
+  left.rn_parent = &child;
+  right.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &child);
+  assert_ptr_equal(child.rn_parent, 0);
+  assert_ptr_equal(child.rn_left, &left);
+  assert_ptr_equal(child.rn_right, &right);
+  assert_ptr_equal(left.rn_parent, &child);
+  assert_ptr_equal(right.rn_parent, &child);
+}
+
+void
+test_rooted_parent_left(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cleft = RB_NODE_INIT(0), cright = RB_NODE_INIT(0);
+  rb_node_t pright = RB_NODE_INIT(0);
+
+  tree.rt_root = &parent;
+  parent.rn_left = &child;
+  parent.rn_right = &pright;
+  child.rn_parent = &parent;
+  child.rn_left = &cleft;
+  child.rn_right = &cright;
+  pright.rn_parent = &parent;
+  cleft.rn_parent = &child;
+  cright.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, &cright);
+  assert_ptr_equal(parent.rn_right, &pright);
+  assert_ptr_equal(child.rn_parent, 0);
+  assert_ptr_equal(child.rn_left, &cleft);
+  assert_ptr_equal(child.rn_right, &parent);
+  assert_ptr_equal(pright.rn_parent, &parent);
+  assert_ptr_equal(cleft.rn_parent, &child);
+  assert_ptr_equal(cright.rn_parent, &parent);
+}
+
+void
+test_rooted_parent_left_nograndchild(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cleft = RB_NODE_INIT(0);
+  rb_node_t pright = RB_NODE_INIT(0);
+
+  tree.rt_root = &parent;
+  parent.rn_left = &child;
+  parent.rn_right = &pright;
+  child.rn_parent = &parent;
+  child.rn_left = &cleft;
+  pright.rn_parent = &parent;
+  cleft.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, 0);
+  assert_ptr_equal(parent.rn_right, &pright);
+  assert_ptr_equal(child.rn_parent, 0);
+  assert_ptr_equal(child.rn_left, &cleft);
+  assert_ptr_equal(child.rn_right, &parent);
+  assert_ptr_equal(pright.rn_parent, &parent);
+  assert_ptr_equal(cleft.rn_parent, &child);
+}
+
+void
+test_rooted_parent_right(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cleft = RB_NODE_INIT(0), cright = RB_NODE_INIT(0);
+  rb_node_t pleft = RB_NODE_INIT(0);
+
+  tree.rt_root = &parent;
+  parent.rn_left = &pleft;
+  parent.rn_right = &child;
+  child.rn_parent = &parent;
+  child.rn_left = &cleft;
+  child.rn_right = &cright;
+  pleft.rn_parent = &parent;
+  cleft.rn_parent = &child;
+  cright.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, &pleft);
+  assert_ptr_equal(parent.rn_right, &cleft);
+  assert_ptr_equal(child.rn_parent, 0);
+  assert_ptr_equal(child.rn_left, &parent);
+  assert_ptr_equal(child.rn_right, &cright);
+  assert_ptr_equal(pleft.rn_parent, &parent);
+  assert_ptr_equal(cleft.rn_parent, &parent);
+  assert_ptr_equal(cright.rn_parent, &child);
+}
+
+void
+test_rooted_parent_right_nograndchild(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cright = RB_NODE_INIT(0);
+  rb_node_t pleft = RB_NODE_INIT(0);
+
+  tree.rt_root = &parent;
+  parent.rn_left = &pleft;
+  parent.rn_right = &child;
+  child.rn_parent = &parent;
+  child.rn_right = &cright;
+  pleft.rn_parent = &parent;
+  cright.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, &pleft);
+  assert_ptr_equal(parent.rn_right, 0);
+  assert_ptr_equal(child.rn_parent, 0);
+  assert_ptr_equal(child.rn_left, &parent);
+  assert_ptr_equal(child.rn_right, &cright);
+  assert_ptr_equal(pleft.rn_parent, &parent);
+  assert_ptr_equal(cright.rn_parent, &child);
+}
+
+void
+test_left_parent_left_nograndchild(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t root = RB_NODE_INIT(0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cleft = RB_NODE_INIT(0);
+  rb_node_t pright = RB_NODE_INIT(0);
+
+  tree.rt_root = &root;
+  root.rn_left = &parent;
+  parent.rn_parent = &root;
+  parent.rn_left = &child;
+  parent.rn_right = &pright;
+  child.rn_parent = &parent;
+  child.rn_left = &cleft;
+  pright.rn_parent = &parent;
+  cleft.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_ptr_equal(root.rn_left, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, 0);
+  assert_ptr_equal(parent.rn_right, &pright);
+  assert_ptr_equal(child.rn_parent, &root);
+  assert_ptr_equal(child.rn_left, &cleft);
+  assert_ptr_equal(child.rn_right, &parent);
+  assert_ptr_equal(pright.rn_parent, &parent);
+  assert_ptr_equal(cleft.rn_parent, &child);
+}
+
+void
+test_right_parent_right_nograndchild(void **state)
+{
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t root = RB_NODE_INIT(0);
+  rb_node_t parent = RB_NODE_INIT(0), child = RB_NODE_INIT(0);
+  rb_node_t cright = RB_NODE_INIT(0);
+  rb_node_t pleft = RB_NODE_INIT(0);
+
+  tree.rt_root = &root;
+  root.rn_right = &parent;
+  parent.rn_parent = &root;
+  parent.rn_left = &pleft;
+  parent.rn_right = &child;
+  child.rn_parent = &parent;
+  child.rn_right = &cright;
+  pleft.rn_parent = &parent;
+  cright.rn_parent = &child;
+
+  _rb_rotate(&tree, &child);
+
+  assert_ptr_equal(tree.rt_root, &root);
+  assert_ptr_equal(root.rn_right, &child);
+  assert_ptr_equal(parent.rn_parent, &child);
+  assert_ptr_equal(parent.rn_left, &pleft);
+  assert_ptr_equal(parent.rn_right, 0);
+  assert_ptr_equal(child.rn_parent, &root);
+  assert_ptr_equal(child.rn_left, &parent);
+  assert_ptr_equal(child.rn_right, &cright);
+  assert_ptr_equal(pleft.rn_parent, &parent);
+  assert_ptr_equal(cright.rn_parent, &child);
+}
+
 int
 main(void)
 {
   const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_noparent),
+    cmocka_unit_test(test_rooted_parent_left),
+    cmocka_unit_test(test_rooted_parent_left_nograndchild),
+    cmocka_unit_test(test_rooted_parent_right),
+    cmocka_unit_test(test_rooted_parent_right_nograndchild),
+    cmocka_unit_test(test_left_parent_left_nograndchild),
+    cmocka_unit_test(test_right_parent_right_nograndchild)
   };
 
   return cmocka_run_group_tests_name("Test _rb_rotate.c", tests, 0, 0);
