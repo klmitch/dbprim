fc233f4372935aaaf50be2220a7a46af4fc2940e
Kevin L. Mitchell <klmitch@mit.edu>
2017-12-05 22:11:02 -0600

    Begin sketching in tests for rt_add()
    This commit creates the initial test cases for rt_add(), testing all the
    sanity checks at the beginning of the function.

--- tests/test_rt_add.c
+++ tests/test_rt_add.c
@@ -40,10 +40,133 @@ __wrap__rb_rotate(rb_tree_t *tree, rb_node_t *child)
   return (void)mock();
 }
 
+void
+test_zerotree(void **state)
+{
+  db_err_t err;
+  rb_node_t node = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  err = rt_add(0, &node, &key);
+
+  assert_int_equal(err, DB_ERR_BADARGS);
+}
+
+void
+test_badtree(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  tree.rt_magic = 0;
+
+  err = rt_add(&tree, &node, &key);
+
+  assert_int_equal(err, DB_ERR_BADARGS);
+}
+
+void
+test_zeronode(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  err = rt_add(&tree, 0, &key);
+
+  assert_int_equal(err, DB_ERR_BADARGS);
+}
+
+void
+test_badnode(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  node.rn_magic = 0;
+
+  err = rt_add(&tree, &node, &key);
+
+  assert_int_equal(err, DB_ERR_BADARGS);
+}
+
+void
+test_zerokey(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+
+  err = rt_add(&tree, &node, 0);
+
+  assert_int_equal(err, DB_ERR_BADARGS);
+}
+
+void
+test_busy(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  node.rn_tree = &tree;
+
+  err = rt_add(&tree, &node, &key);
+
+  assert_int_equal(err, DB_ERR_BUSY);
+}
+
+void
+test_frozen(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  tree.rt_flags |= RBT_FLAG_FREEZE;
+
+  err = rt_add(&tree, &node, &key);
+
+  assert_int_equal(err, DB_ERR_FROZEN);
+}
+
+void
+test_duplicate(void **state)
+{
+  db_err_t err;
+  rb_tree_t tree = RB_TREE_INIT(0, 0);
+  rb_node_t node = RB_NODE_INIT(0);
+  rb_node_t other = RB_NODE_INIT(0);
+  db_key_t key = DB_KEY_INIT("spam", 4);
+
+  will_return(__wrap__rb_locate, &other);
+  expect_value(__wrap__rb_locate, tree, &tree);
+  expect_value(__wrap__rb_locate, node, &node);
+  expect_value(__wrap__rb_locate, key, &key);
+
+  err = rt_add(&tree, &node, &key);
+
+  assert_int_equal(err, DB_ERR_DUPLICATE);
+}
+
 int
 main(void)
 {
   const struct CMUnitTest tests[] = {
+    cmocka_unit_test(test_zerotree),
+    cmocka_unit_test(test_badtree),
+    cmocka_unit_test(test_zeronode),
+    cmocka_unit_test(test_badnode),
+    cmocka_unit_test(test_zerokey),
+    cmocka_unit_test(test_busy),
+    cmocka_unit_test(test_frozen),
+    cmocka_unit_test(test_duplicate)
   };
 
   return cmocka_run_group_tests_name("Test rt_add.c", tests, 0, 0);
